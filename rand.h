7f1ba881d6d0dba6796b1015bfd129dae2bee2d3
Son Pham <snowru@gmail.com>
2024-12-30 19:30:18 -0800

    update

--- rand.h
+++ rand.h
@@ -1,5 +1,6 @@
 #ifndef RAND_H
 #define RAND_H
+#include<stdlib.h>
 /*
 Implement Mersenne Twister algorithm for generating random numbers
 The basis of Mersenne Twister algorithm stems from the Linear Feedback Shift Register. 
@@ -10,4 +11,27 @@ period of the sequence reaches the maximum length of 2^n -1. Note that a sequenc
 The algorithm operates on a 624 elements, each with 32 bits. The 31 bits from positions [1, 31] are not used,
 thus, n = 624 * 32 - 31 = 19937. The period of the sequence is 2^19937 -1, a prime number (Mersenne prime number) - thus the name.
 */
+struct rand
+{
+    uint32_t states[624];
+    int index;
+};
+
+void initialize_rand(struct rand *r,uint32_t seed){
+    r->states[0] = seed;
+    for(int i = 1; i < 624; i++){
+        r->states[i] = 0x6c078965 * (r->states[i-1] ^ (r->states[i-1] >> 30)) + i;
+    }
+    r->index = 0;
+}
+void generate_numbers(struct rand *r){
+    for(int i = 0; i < 624; i++){
+        uint32_t y = (r->states[i] & 0x80000000) + (r->states[ (i +1) % 624] & 0x7fffffff);
+        r->states[i] = r->states[(i + 397) % 624] ^ (y >> 1);
+        if (y %2 != 0){
+            r->states[i] ^= 0x9908b0df;
+        }
+    }
+}
+
 #endif
