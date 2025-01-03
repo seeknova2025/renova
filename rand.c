d60807a080f3ab13accd60679b212b0058046d93
Son Pham <snowru@gmail.com>
2025-01-02 16:46:11 -0800

    new way

--- rand.c
+++ rand.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 int main(){
     struct rand r;
-    uint32_t seed = 47;
+    uint32_t seed = 137;
     initialize_rand(&r, seed);
-    uint32_t n = extract_number(&r);
-    printf("random number: %u\n", n);
+    for(int i = 0; i < 10; i++){
+        uint32_t n = randint32(&r);
+        printf("random number: %u\n", n);
+    }
+    //create 10 random 64bit numbers
+    for (int i = 0; i < 10; i++){
+        uint64_t n  = randint64(&r);
+        printf("64 bits: %llu\n", n);
+    }
     return 0;
     
 }
